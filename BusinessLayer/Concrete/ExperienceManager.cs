e8842ce53a9fa2a92b245bf37d36fc30a471feb2
Melih ÖZASLAN <61635941+MelihOzaslan01@users.noreply.github.com>
2023-09-28 14:25:58 +0300

    Add files via upload
    BusinessLayer katmanı yüklendi

---
+++ BusinessLayer/Concrete/ExperienceManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class ExperienceManager : IExperienceService
+	{
+		IExperienceDal ExperienceDal;
+
+		public ExperienceManager(IExperienceDal experienceDal)
+		{
+			ExperienceDal = experienceDal;
+		}
+
+		public void TAdd(Experience t)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void TDelete(Experience t)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Experience TGetByID(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Experience> TGetList()
+		{
+			return ExperienceDal.GetList();
+		}
+
+		public void TUpdate(Experience t)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ BusinessLayer/Concrete/PortfolioManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class PortfolioManager : IPortfolioService
+	{
+		IPortfolioDal _PortfolioDal;
+
+		public PortfolioManager(IPortfolioDal PortfolioDal)
+		{
+			_PortfolioDal = PortfolioDal;
+		}
+
+		public void TAdd(Portfolio t)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void TDelete(Portfolio t)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Portfolio TGetByID(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Portfolio> TGetList()
+		{
+			return _PortfolioDal.GetList();
+		}
+
+		public void TUpdate(Portfolio t)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
